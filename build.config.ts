0c3bb99e457427887361194af3615c01c8f32406
jianzhongmin <282126990@qq.com>
2022-08-30 18:46:09 +0800

    fix:unbuild

--- build.config.ts
+++ build.config.ts
@@ -2,18 +2,22 @@ import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
   entries: [
-    // default
-    './src/index',
     // mkdist builder transpiles file-to-file keeping original sources structure
     {
       builder: 'mkdist',
       input: './src/components/',
       outDir: './dist/components'
     },
+    {
+      builder: 'mkdist',
+      input: './src/utils/',
+      outDir: './dist/utils'
+    },
   ],
-  clean: true,
   // Generates .d.ts declaration file
   declaration: true,
+  // 不退出命令
+  failOnWarn: false,
   // rollup
   rollup: {
     // 生成cjs文件
